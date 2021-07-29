fa80d8878df780369d1671efa8abfc0bc846c946
JuHyeon-S <seo99832@daum.net>
2021-07-29 12:35:10 +0900

    Update App.js

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let posts = "강남 고기 맛집";
+  
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
+      <img src={logo}/>
+      <h4>{posts}</h4>
     </div>
   );
 }
