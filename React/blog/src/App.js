086e2ae74f205526249ce93f9a3ec4aafe481e6b
JuHyeon-S <seo99832@daum.net>
2021-07-29 12:49:31 +0900

    Update App.js

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+
+  let [title, titleChange] = useState('남자 코트 추천'); // ['남자코트추천', 데이터 변경함수]
+
   let posts = "강남 고기 맛집";
   
   return (
@@ -10,7 +14,7 @@ function App() {
         <div>개발 Blog</div>
       </div>
       <div className="list">
-        <h3>{posts}</h3>
+        <h3>{title}</h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
