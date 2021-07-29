ef97ab35d3575ed6d7faa1f4df227ae875ad4273
JuHyeon-S <seo99832@daum.net>
2021-07-29 13:06:17 +0900

    Update App.js

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
 
   let [title, titleChange] = useState(['리액트 공부 시작','vue?','어렵다']); // ['남자코트추천', 데이터 변경함수]
-
+  let [like, likeChange] = useState(0);
   let posts = "강남 고기 맛집";
   
   return (
@@ -15,7 +15,7 @@ function App() {
         <div>개발 Blog</div>
       </div>
       <div className="list">
-        <h3>{title[0]}</h3>
+        <h3>{title[0]} <span onClick={() =>{ console.log(1)}}>👍</span> {like} </h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
