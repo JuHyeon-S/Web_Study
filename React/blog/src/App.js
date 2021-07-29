1b7abf23dd473e69353df3286718dc53e95863ec
JuHyeon-S <seo99832@daum.net>
2021-07-29 12:59:39 +0900

    Update App.js

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+/* eslint-disable */
 import React,{useState} from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  let [title, titleChange] = useState('남자 코트 추천'); // ['남자코트추천', 데이터 변경함수]
+  let [title, titleChange] = useState(['리액트 공부 시작','vue?','어렵다']); // ['남자코트추천', 데이터 변경함수]
 
   let posts = "강남 고기 맛집";
   
@@ -14,8 +15,18 @@ function App() {
         <div>개발 Blog</div>
       </div>
       <div className="list">
-        <h3>{title}</h3>
+        <h3>{title[0]}</h3>
         <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{title[1]}</h3>
+        <p>2월 23일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{title[2]}</h3>
+        <p>2월 29일 발행</p>
         <hr/>
       </div>
     </div>
